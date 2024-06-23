82e788a9e2ef569face7dd71f188d7fdd4660e8d
Shuhib1924 <shuhib_s@live.de>
2024-06-23 17:59:13 +0200

    hx-post

--- views/index.js
+++ views/index.js
@@ -23,6 +23,21 @@ const createHomepageTemplate = () => /*html*/`
         <div class="add-book-form">
           <h2>What do you want to read?</h2>
           <!-- form template here later -->
+            <form>
+            <input
+              id="title"
+              name="title"
+              placeholder="title"
+              type="text"
+            />
+            <input
+              id="author"
+              name="author"
+              placeholder="author"
+              type="text"
+            />
+            <button hx-post="/books" hx-target=".book-list ul" hx-swap="beforeend">Add</button>
+          </form>
         </div>
       </main>
     </body>
